ebceff093ec62a66ee3ec9dce2a0f0dadab350ed
dries11 <daniel.ries@ymail.com>
2017-02-25 22:39:53 -0500

    Adde UML

--- src/main/java/ries/dan/DanLinkedList.java
+++ src/main/java/ries/dan/DanLinkedList.java
@@ -5,11 +5,21 @@ package ries.dan;
  */
 public class DanLinkedList<T>{
 
+    private int listLength;
+    private Node<T> head;
+
+
+    public DanLinkedList(){
+        this.listLength = 0;
+    }
+
+    public void add(T data){
+
+    }
+
     class Node<T>{
         private T data;
         private Node next;
-
-
 
         public Node(T data){
             this.data = data;
